41779f40c4e9ed93fc4b6364090631b4ee99167b
carlosgsouza <carlosgsouza@gmail.com>
2013-09-08 22:11:42 -0300

    reviewing project description

--- GrailsKarmaGrailsPlugin.groovy
+++ GrailsKarmaGrailsPlugin.groovy
@@ -1,41 +1,20 @@
 class GrailsKarmaGrailsPlugin {
-    // the plugin version
-    def version = "0.1.11-SNAPSHOT"
-    // the version or versions of Grails the plugin is designed for
+    def version = "0.1.12-SNAPSHOT"
     def grailsVersion = "1.3 > *"
-    // the other plugins this plugin depends on
     def dependsOn = [:]
-    // resources that are excluded from plugin packaging
-    def pluginExcludes = [
-        "grails-app/views/error.gsp"
-    ]
-
-    def title = "Grails Karma Plugin" // Headline display name of the plugin
+    def pluginExcludes = ["grails-app/views/error.gsp"]
+    def title = "Grails Karma Plugin"
     def author = "Carlos Souza"
     def authorEmail = "carlosgsouza@gmail.com"
     def description = '''\
-Integrates Grails with the Karma Test Runner for JavaScript automated tests.
+Run Javascript tests on the Grails build with Karma
 '''
+    def documentation = "http://carlosgsouza.github.io/grails-karma/"
 
-    // URL to the plugin's documentation
-    def documentation = "https://github.com/carlosgsouza/grails-karma"
-
-    // Extra (optional) plugin metadata
-
-    // License: one of 'APACHE', 'GPL2', 'GPL3'
     def license = "APACHE"
-
-    // Details of company behind the plugin (if there is one)
-//    def organization = [ name: "My Company", url: "http://www.my-company.com/" ]
-
-    // Any additional developers beyond the author specified above.
-    def developers = [ [ name: "Daniel Fernandes", email: "danielpsf@gmail.com" ]]
-
-    // Location of the plugin's issue tracker.
+    def developers = [ [ name: "Carlos Souza", email: "carlosgsouza@gmail.com" ], [ name: "Daniel Fernandes", email: "danielpsf@gmail.com" ] ]
     def issueManagement = [ system: "GitHub", url: "https://github.com/carlosgsouza/grails-karma/issues" ]
-
-    // Online location of the plugin's browseable source code.
-//    def scm = [ url: "http://svn.codehaus.org/grails-plugins/" ]
+	def scm = [ url: "https://github.com/carlosgsouza/grails-karma" ]
 
     def doWithWebDescriptor = { xml ->
         // TODO Implement additions to web.xml (optional), this event occurs before

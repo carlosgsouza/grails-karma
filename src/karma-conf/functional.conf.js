143626a30a1f26e6ce8ee33c9630cf6d75b0ffb4
Rodrigo Guimaraes <rodrigo@agileoperatios.com.br>
2013-10-25 14:31:03 -0200

    >Changed karma functional tests configuration to be in compliance with karma version 0.10.x

--- src/karma-conf/functional.conf.js
+++ src/karma-conf/functional.conf.js
@@ -1,76 +1,80 @@
-// Testacular configuration
-
-// base path, that will be used to resolve files and exclude
-basePath = '../../';
-
-
-// list of files / patterns to load in the browser
-files = [
-	ANGULAR_SCENARIO,
-	ANGULAR_SCENARIO_ADAPTER,
-	'web-app/js/**/*.js',
-	'test/js-e2e/**/*.js'
-];
-
-// list of files to exclude
-exclude = [
-];
-
-
-// test results reporter to use
-// possible values: 'dots', 'progress', 'junit'
-// junit is required in order for grails-karma to work
-reporters = ['progress', 'junit'];
-
-junitReporter = { 
-	// will be resolved to basePath (in the same way as files/exclude patterns) 
-	outputFile: 'target/e2e/test-results.xml'
+// Karma configuration for unit tests
+module.exports = function(config) {
+ config.set({
+  // base path, that will be used to resolve files and exclude
+  basePath: '../../../',
+  
+  //frameworks to use
+  frameworks: ['ng-scenario'],
+  
+  // list of files / patterns to load in the browser
+  files: [
+   'web-app/js/**/*.js',
+   'test/js-functional/**/*Scenario.js' // can't touch this!
+  ],
+  
+  //list of files to exclude
+  exclude: [
+  ],
+  
+  
+  // test results reporter to use
+  // possible values: 'dots', 'progress', 'junit', 'growl', 'coverage'
+  // junit is required in order for grails-karma to work
+  reporters: ['progress', 'junit'],
+  
+  junitReporter: { 
+   // will be resolved to basePath (in the same way as files/exclude patterns) 
+   outputFile: 'target/test-reports/karma/functional-test-results.xml'
+  },
+  
+  //web server port
+  port: 3561,
+  
+  
+  //Proxies the root URL so the browser's pages can be accessed
+  proxies: {
+      '/': 'http://localhost:8080/'
+  },
+  
+  
+  // enable / disable colors in the output (reporters and logs)
+  colors: true,
+  
+  
+  // level of logging
+  // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+  logLevel: config.LOG_INFO,
+  
+  
+  // enable / disable watching file and executing tests whenever any file changes
+  autoWatch: false,
+  
+  
+  // Start these browsers, currently available:
+  // - Chrome
+  // - ChromeCanary
+  // - Firefox
+  // - Opera
+  // - Safari (only Mac)
+  // - PhantomJS
+  // - IE (only Windows)
+  browsers: ['Chrome'],
+  
+  // If browser does not capture in given timeout [ms], kill it
+  captureTimeout: 30000,
+  
+  
+  // Continuous Integration mode
+  // if true, it capture browsers, run tests and exit
+  singleRun: true,
+  
+  plugins: [
+   'karma-jasmine',
+   'karma-ng-scenario',
+   'karma-chrome-launcher',
+   'karma-firefox-launcher',
+   'karma-junit-reporter'
+  ]
+ });
 };
-
-// web server port
-port = 3561;
-
-
-// cli runner port
-runnerPort = 9101;
-
-
-// enable / disable colors in the output (reporters and logs)
-colors = true;
-
-
-// level of logging
-// possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
-logLevel = LOG_INFO;
-
-
-// enable / disable watching file and executing tests whenever any file changes
-autoWatch = true;
-
-
-// Start these browsers, currently available:
-// - Chrome
-// - ChromeCanary
-// - Firefox
-// - Opera
-// - Safari (only Mac)
-// - PhantomJS
-// - IE (only Windows)
-browsers = ['Chrome'];
-
-
-// If browser does not capture in given timeout [ms], kill it
-captureTimeout = 5000;
-
-
-// Continuous Integration mode
-// if true, it capture browsers, run tests and exit
-singleRun = true;
-
-debugger;
-
-plugins = [
-	'testacular-chrome-launcher',
-	'testacular-firefox-launcher',
-	'testacular-junit-reporter'
-];;
